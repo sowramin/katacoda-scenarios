caad54d7738ff4de4a75237cff7e5b594fd98c51
sowramin <sowramin@gmail.com>
2017-07-28 14:35:51 +0530

    Update step1-verify.sh

--- scenario1/step1-verify.sh
+++ scenario1/step1-verify.sh
@@ -1,2 +1,2 @@
-history | tail -n 10 > /home/scrapbook/tutorial/logg.txt
+history | tail -n 10 > /home/scrapbook/tutorial/logg1.txt
 [ $(history | grep -o "docker version" | wc -l | tail -n 30) -gt 1 ] && echo "done"
